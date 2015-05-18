38b7688115fddfe9c94a62a6d8ecb1d05c280958
Justin Combs <combs.justin.m@gmail.com>
2015-05-18 15:30:35 -0400

    layout for changing time types

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-function clock() {
+function hexClock() {
   function check(i) {if (i < 10) {i = "0" + i;} return i;}
   var today = new Date();
   var hour = today.getHours();
@@ -9,11 +9,44 @@ function clock() {
   var $clock = document.querySelector('#clock');
   $clock.innerHTML = '#' + hour + minute + second;
   second = check(second);
-  document.bgColor = '#' + hour + minute + second;
+  document.querySelector('body').style.background = '#' + hour + minute + second;
 }
 
+// function regClock() {
+//   function check(i) {if (i < 10) {i = "0" + i;} return i;}
+//   var today = new Date();
+//   var hour = today.getHours();
+//   var minute = today.getMinutes();
+//   minute = check(minute);
+//   var second = today.getSeconds();
+//   second = check(second);
+//   var $clock = document.querySelector('#clock');
+//   $clock.innerHTML = hour + ':' + minute + ':' + second;
+//   second = check(second);
+//   document.querySelector('body').style.backgroundColor = '#' + hour + minute + second;
+// }
 
+// var regTimer = setInterval(function(){
+//   regClock();
+// }, 500);
 
-setInterval(function(){
-  clock();
+var hexTimer = setInterval(function(){
+    hexClock();
 }, 500);
+
+// function showColor() {
+//   clearInterval(regTimer);
+//   setInterval(function(){
+//   hexClock();
+// }, 500);
+// }
+
+// function showTime() {
+//   clearInterval(hexTimer);
+//   setInterval(function(){
+//   regClock();
+// }, 500);
+// }
+
+// document.querySelector('#showColor').addEventListener('click', showColor);
+// document.querySelector('#showTime').addEventListener('click', showTime);
